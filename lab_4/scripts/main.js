c384c87430c9e5f690981646ed0a4d7fbb088f20
intisar505 <35035930+intisar505@users.noreply.github.com>
2020-03-04 22:13:23 -0500

    Delete main.js

--- lab_4/scripts/main.js
+++
@@ -1,7 +0,0 @@
-let firstname = "Intisar";
-let lastname = "Muhammad";
-let firstandlast = "Intisar Muhammad";
-let split = firstandlast.split();
-let name_length = split[0].length + split[1].length;
-const header = document.querySelector('.header');
-header.innerHTML = firstandlast +" 's " + name_length + "Lab 4";
