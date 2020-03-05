85667c5c4fd736c0692a4b9b56c9ef4dc8fab12c
intisar505 <35035930+intisar505@users.noreply.github.com>
2020-03-04 19:18:17 -0500

    mdn example

--- lab_4/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const myHeading = document.querySelector('.header');
-myHeading.textContent = "Intisar Muhammad's Lab 4";
-
-var p1 = document.createElement("P");
-var txt1 = document.createTextNode("My name has 15 characters");
-p1.appendChild(txt1);
-document.querySelector('.content').appendChild(p1);
